8a34625a427e0c6601869d51ce1ac66c6238fbd0
Luca Belardinelli <belardinelliluca@gmail.com>
2024-12-20 10:15:35 +0100

    [bonus]

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,13 +15,25 @@ console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-  const longNames = teachers.length;
+  const longNames = [];
+  for (let i=0; i < teachers.length; i++){
+    let numberteacher = teachers[i];
+    let name = numberteacher.length;
+    if (name >=5)
+      longNames.push(numberteacher)
+  }
+
+  console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(1,1)
+console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes('Fabio');
+console.log(isFabioPresent)
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join();
+console.log(teachersString);
