71777c1ad3a3d0f2e705cdde519d53cfcedc1802
Luca Belardinelli <belardinelliluca@gmail.com>
2024-12-20 10:28:57 +0100

    [correction bonus]

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,30 +10,40 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
+
 const reversedTeachers = teachers.toReversed();
 console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
+
   const longNames = [];
   for (let i=0; i < teachers.length; i++){
     let numberteacher = teachers[i];
     let name = numberteacher.length;
-    if (name >=5)
-      longNames.push(numberteacher)
+    if (name >=5){
+      longNames.push(numberteacher);
+    }
   }
-
   console.log(longNames)
 
+// versione filtro
+
+// const longNames - teachers.filter((numberteacher) -> numberteacher.length >- 5);
+// console.log(longNames);
+
 // 3. Rimuovi 'Ed' dall'array teachers
+
 teachers.splice(1,1)
 console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+
 const isFabioPresent = teachers.includes('Fabio');
 console.log(isFabioPresent)
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+
 const teachersString = teachers.join();
 console.log(teachersString);
